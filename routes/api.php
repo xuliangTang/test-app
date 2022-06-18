2302babde272e93125c982cccc5dc96c5df79e3e
Lily <1297441127@qq.com>
2022-06-18 14:50:32 +0800

    feat: get user

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/user', function (Request $request) {
-   return response()->json(['code' => 200, 'msg' => 'success']);
+   return response()->json(['code' => 200, 'msg' => 'success', 'data' => ['id' => 1]]);
 });
 
