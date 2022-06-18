42ef551ca201b0c53fa96211d9aa8c049bf63212
Lily <1297441127@qq.com>
2022-06-18 14:45:43 +0800

    🎉 Initial Commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUser()
+    {
+        $response = $this->get('/api/user');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'code', 'msg'
+            ]);
+    }
+}
